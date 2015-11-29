0b2f76242814589382f4893426f7cf34403e6fe6
Tomas Doran <tdoran@yelp.com>
2015-11-29 01:08:31 +0000

    Egress allow all rule also

--- main.tf
+++ main.tf
@@ -42,6 +42,12 @@ resource "aws_security_group" "allow_all" {
       protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 module "instances" {
